6eb2246530bf12f7ce84d34058dbc509bded5047
ansan <anibalsantosgo@gmail.com>
2019-09-26 00:13:08 +0200

    js

---
+++ 00 - Introduction to programming in JavaScript/02 - PECS/05 - Numbers, Dates, Text, Arrays/PEC 5 - Numbers, Dates, Text, Arrays.js
@@ -0,0 +1,32 @@
+/*
+
+1 . Escribid una función en JavaScript que devuelva la fecha actual en formato de texto "día de mes de año".
+
+Ejemplo: si hoy es 13/08/2018, la salida debe ser "13 de agosto de 2018".
+
+2. Escribid una función en JavaScript que devuelva la fecha anterior a una fecha pasada como parámetro en formato de texto 'DD/MM/AAAA', 
+donde DD es el día, MM el mes y AAAA el año.
+
+Ejemplo: si la entrada es '01/01/2019', la salida debe ser '31/12/2018'.
+
+3. Escribid un programa en JavaScript que, dada una frase, la transforme y escriba por consola de manera que después de cada vocal se añada una "p" 
+y a continuación se repita la vocal.
+
+Ejemplo: si la entrada és "Hola qué tal", la salida debe ser "Hopolapa qupuépé tapal"
+
+4. Escribid una función en JavaScript que ordene un array de números mediante ordenación de burbuja. 
+Escribid otra función que compruebe que un array de números está ordenado: debe devolver True si están ordenados o False si no lo están. Escribid un programa en JavaScript que genere 1000 números aleatorios, y use las funciones anteriores para ordenarlos y comprobar que están ordenados, a continuación mostrarlos por consola.
+
+Podeís consultar la wikipedia:
+
+https://es.wikipedia.org/wiki/Ordenamiento_de_burbuja
+
+5. Escribid un programa en JavaScript que muestre por consola un triángulo de Pascal ( o triángulo de Tartaglia) de 20 filas. 
+La primera fila sólo tiene un 1. La segunda fila tiene dos 1s. 
+En las siguientes filas, los números son el resultado de sumar los dos números inmediatamiente superiores. Los números situados en los laterales siempre son 1.
+
+Podéis consultar la wikipedia:
+
+https://es.wikipedia.org/wiki/Tri%C3%A1ngulo_de_Pascal
+
+*/
---
+++ 00 - Introduction to programming in JavaScript/02 - PECS/06 - Objects/PEC 6 - Objects.js
@@ -0,0 +1,43 @@
+/*
+
+1 . Escribid un programa en JavaScript que cree un array de objetos, donde cada objeto describa un libro con las propiedades siguientes: 
+título, autor y código ISBN. Escribid una función que itere sobre todos los libros, y para cada uno escriba por consola el título, el autor, el código ISBN, 
+y si éste empieza por 84 o 97884, además escriba "ISBN España".
+
+2. Escribid una función en JavaScript que muestre por consola las propiedades y los métodos de un objeto.
+
+Por ejemplo, dado un objeto construído así:
+
+  function Auto(marca, modelo, potencia, antiguedad, precio) {
+    this.marca = marca;
+    this.modelo = modelo;
+    this.potencia = potencia;
+    this.antiguedad = antiguedad;
+    this.precio = precio;
+    this.edadAprox = function (anio) {
+      return (anio - this.antiguedad);
+    }
+  }
+
+  var coche=new Auto("Seat", "Ibiza", 90, 2010, 5000);
+
+La función del ejercicio, pasándole como argumento la variable coche, tendría que escribir por consola algo parecido a ésto:
+
+  Propiedades: marca, modelo, potencia, antiguedad, precio
+  Métodos: edadAprox
+
+3. Escribid una función en JavaScript que dado un objeto, devuelva otro donde los nombres de las propiedades se han transformado en valores, 
+y los valores en nombres de las propiedades.
+
+Ejemplo: dado el objeto { 0: 'a', 1: 'b', 2: 'c' }, la función podría devolver el objeto { 'a': 0, 'b': 1, 'c': 2 }.
+
+4. Escribid una función en JavaScript que analice una URL y devuelva un objecto con las siguientes propiedades: protocolo, hot, query y un array params.
+
+Ejemplo: Si la entrada es “http://cv.uoc.edu/webapps/bustiaca/listMails.do?mailId=5848042_774330153&l=181_m4_250_01_t01”, la salida debe ser:
+{ "protocolo": "http", "host": "cv.uoc.edu", "path": "/webapps/bustiaca/listMails.do", "query": {"mailId": "5848042_774330153", "l": "181_m4_250_01_t01"} }
+
+5. Escribid una función en JavaScript que convierta un objeto a formato JSON, sin usar la función JSON.stringify().
+
+
+
+*/
