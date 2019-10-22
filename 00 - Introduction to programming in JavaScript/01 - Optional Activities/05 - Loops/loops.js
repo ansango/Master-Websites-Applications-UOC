a1e3f95b45611b94a256cfe1669383c40d03b9e2
ansan <anibalsantosgo@gmail.com>
2019-10-22 18:22:14 +0200

    js

---
+++ 00 - Introduction to programming in JavaScript/01 - Optional Activities/05 - Loops/loops.js
@@ -0,0 +1,35 @@
+/*
+// Prueba de bucle con for
+for (let i = 1; i < 5; i++){
+  console.log(i);
+*/
+
+/*
+Modifica el bucle que hemos presentado de ejemplo, para que escriba los números al revés, de 4 a 1. 
+*/
+
+for (let i = 1; i < 5; i--) {
+    console.log(i)
+}
+
+/*
+Modifica el bucle que hemos presentado de ejemplo, para que escriba los números del 50 al 54.
+*/
+
+/*
+Modifica el bucle que hemos presentado de ejemplo, para que escriba los números del 1 al 7 pero de 2 en 2. En la práctica esto se puede hacer de dos maneras:
+- incrementando la variable del for en dos en vez de uno. ¿Sabrías hacerlo?
+- poniendo un if dentro del bucle para que solo se escriban los números impares. ¿Sabrías hacerlo?
+*/
+
+/*
+Reflexiona sobre las dos opciones anteriores. ¿Cuál es mejor? ¿Por qué?
+*/
+
+/*
+Hacer una tabla de multiplicar usando los bucles es fácil... Haz la tabla de multiplicar del 4.
+*/
+
+/*
+Un poco más complicado... ¿Cómo harías para escribir las tablas de multiplicar del 1 al 10?. Una pista, necesitarás dos bucles, uno dentro de otro.
+*/
