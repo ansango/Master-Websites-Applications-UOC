25f990b7c3498c5b0a52254da518077bd1280fee
ansan <anibalsantosgo@gmail.com>
2019-12-18 16:06:12 +0100

    js

--- 00 - Introduction to programming in JavaScript/02 - PECS/05 - Numbers, Dates, Text, Arrays/PEC 5 - Numbers, Dates, Text, Arrays.js
+++ 00 - Introduction to programming in JavaScript/02 - PECS/05 - Numbers, Dates, Text, Arrays/PEC 5 - Numbers, Dates, Text, Arrays.js
@@ -4,22 +4,89 @@
 
 Ejemplo: si hoy es 13/08/2018, la salida debe ser "13 de agosto de 2018".
 
+
+*/
+
+let today = new Date()
+let day = today.getDate()
+let month = today.getMonth()
+let year = today.getFullYear()
+
+switch (month) {
+  case 1:
+  month = 'enero'
+  break;
+  case 2:
+  month = 'febrero'
+  break;
+  case 3:
+  month = 'marzo'
+  break;
+  case 4:
+  month = 'abril'
+  break;
+  case 5:
+  month = 'mayo'
+  break;
+  case 6:
+  month = 'junio'
+  break;
+  case 7:
+  month = 'julio'
+  break;
+  case 8:
+  month = 'agosto'
+  break;
+  case 9:
+  month = 'septiembre'
+  break;
+  case 10:
+  month = 'octubre'
+  break;
+  case 11:
+  month = 'noviembre'
+  break;
+  case 12:
+  month = 'diciembre'
+  break;
+}
+
+console.log(`${day} de ${month} de ${year}`)
+
+
+/*
+
 2. Escribid una función en JavaScript que devuelva la fecha anterior a una fecha pasada como parámetro en formato de texto 'DD/MM/AAAA', 
 donde DD es el día, MM el mes y AAAA el año.
 
 Ejemplo: si la entrada es '01/01/2019', la salida debe ser '31/12/2018'.
+
+*/
+
+
+
+/*
 
 3. Escribid un programa en JavaScript que, dada una frase, la transforme y escriba por consola de manera que después de cada vocal se añada una "p" 
 y a continuación se repita la vocal.
 
 Ejemplo: si la entrada és "Hola qué tal", la salida debe ser "Hopolapa qupuépé tapal"
 
+*/
+
+/*
+
 4. Escribid una función en JavaScript que ordene un array de números mediante ordenación de burbuja. 
-Escribid otra función que compruebe que un array de números está ordenado: debe devolver True si están ordenados o False si no lo están. Escribid un programa en JavaScript que genere 1000 números aleatorios, y use las funciones anteriores para ordenarlos y comprobar que están ordenados, a continuación mostrarlos por consola.
+Escribid otra función que compruebe que un array de números está ordenado: debe devolver True si están ordenados o False si no lo están. Escribid un programa en JavaScript que genere 1000 números aleatorios,
+y use las funciones anteriores para ordenarlos y comprobar que están ordenados, a continuación mostrarlos por consola.
 
 Podeís consultar la wikipedia:
 
 https://es.wikipedia.org/wiki/Ordenamiento_de_burbuja
+
+*/
+
+/*
 
 5. Escribid un programa en JavaScript que muestre por consola un triángulo de Pascal ( o triángulo de Tartaglia) de 20 filas. 
 La primera fila sólo tiene un 1. La segunda fila tiene dos 1s. 
