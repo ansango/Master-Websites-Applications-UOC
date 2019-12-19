71b2965d9e9e4e3000089c17eb1609d911441137
ansan <anibalsantosgo@gmail.com>
2019-12-19 16:27:40 +0100

    js

--- 00 - Introduction to programming in JavaScript/02 - PECS/05 - Numbers, Dates, Text, Arrays/PEC 5 - Numbers, Dates, Text, Arrays.js
+++ 00 - Introduction to programming in JavaScript/02 - PECS/05 - Numbers, Dates, Text, Arrays/PEC 5 - Numbers, Dates, Text, Arrays.js
@@ -158,3 +158,37 @@ https://es.wikipedia.org/wiki/Tri%C3%A1ngulo_de_Pascal
 https://medium.com/javascript-in-plain-english/algorithms-101-pascals-triangle-in-javascript-9fbd9032871
 
 */
+
+let numRows = 5
+
+var generate = function(numRows) {
+    let triangle = [[1], [1,1]]
+    
+    if(numRows === 0){
+        return []
+    } else if(numRows == 1){
+        return [[1]]       
+    } else if(numRows == 2){
+        return  [[1], [1,1]]
+    } else {      
+        for(let i = 2; i < numRows; i++){       
+            addRow(triangle)
+        }
+    }
+    return triangle
+    
+};
+var addRow = function(triangle){
+    let previous = triangle[triangle.length - 1]
+    let newRow = [1]
+    for(let i = 0; i < previous.length - 1; i++){
+        let current = previous[i]
+        let next = previous[i+1] 
+        newRow.push(current + next)
+    }
+    newRow.push(1)
+    return triangle.push(newRow)
+    
+}
+
+generate(numRows)
