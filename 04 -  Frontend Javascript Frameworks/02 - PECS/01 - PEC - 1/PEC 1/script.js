e4c0d3d6bf340231fd119593e344cb6540f927e3
ansango <anibalsantosgo@gmail.com>
2020-09-18 20:36:58 +0200

    pec ej 1

--- 04 -  Frontend Javascript Frameworks/02 - PECS/01 - PEC - 1/PEC 1/script.js
+++ 04 -  Frontend Javascript Frameworks/02 - PECS/01 - PEC - 1/PEC 1/script.js
@@ -23,47 +23,72 @@ function showSuccess(input) {
 
 // Check Email
 
-function isValidEmail(email) {
+function checkEmail(input) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    if (re.test(input.value.trim())) {
+        showSuccess(input)
+    } else {
+        showError(input, 'Email is not valid')
+    }
 }
 
-function isValidPassword(password) {
+// Check required fields
 
+function checkRequired(inputArr) {
+    inputArr.forEach((input) => {
+        if (input.value.trim() === '') {
+            showError(input, `${getFieldName(input)} is required`)
+        } else {
+            showSuccess(input)
+        }
+    });
 }
 
+// Check input length
+
+function checkLength(input, min, max) {
+    if (input.value.length < min) {
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`)
+    } else if (input.value.length > max) {
+        showError(input, `${getFieldName(input)} must be less than ${max} characters`)
+    } else {
+        showSuccess(input)
+    }
+}
+
+// Check passwords match
+
+function checkPasswordsMatch(input1, input2) {
+    if (input1.value !== input2.value) {
+        showError(input2, 'Passwords do not match')
+    }
+}
+
+function checkPasswordReg(input) {
+    const re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/
+
+    if (re.test(input.value.trim())) {
+        showSuccess(input)
+    } else {
+        showError(input, 'Must be a lower and upper case, special char and a number')
+    }
+}
+
+// Get Field Name
+
+function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1)
+}
 
 // Event Listeners
 
 form.addEventListener('submit', function(event) {
     event.preventDefault()
 
-
-    if (username.value === '') {
-        showError(username, 'Username is required')
-    } else {
-        showSuccess(username)
-    }
-
-    if (email.value === '') {
-        showError(email, 'Email is required')
-    } else if (!isValidEmail(email.value)) {
-        showError(email, 'Email is not valid')
-    } else {
-        showSuccess(email)
-    }
-
-    if (password.value === '') {
-        showError(password, 'Password is required')
-    } else if (!isValidPassword(password.value)) {
-        showError(password, 'Password is not valid')
-    } else {
-        showSuccess(password)
-    }
-
-    if (password2.value === '') {
-        showError(password2, 'Confirm password is required')
-    } else {
-        showSuccess(password2)
-    }
+    checkRequired([username, email, password, password2])
+    checkLength(username, 3, 15)
+    checkLength(password, 6, 25)
+    checkEmail(email)
+    checkPasswordsMatch(password, password2)
+    checkPasswordReg(password)
 })
