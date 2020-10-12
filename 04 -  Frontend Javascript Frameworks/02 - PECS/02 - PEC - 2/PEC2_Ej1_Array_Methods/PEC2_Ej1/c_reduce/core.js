bb7b1ca42398bdb7d6b4b710edd76867a76d023d
anibalsantos <anibalsantosgo@gmail.com>
2020-10-12 12:52:05 +0200

    js

--- 04 -  Frontend Javascript Frameworks/02 - PECS/02 - PEC - 2/PEC2_Ej1_Array_Methods/PEC2_Ej1/c_reduce/core.js
+++ 04 -  Frontend Javascript Frameworks/02 - PECS/02 - PEC - 2/PEC2_Ej1_Array_Methods/PEC2_Ej1/c_reduce/core.js
@@ -1,17 +1,24 @@
 function sum(array) {
-  const reducer = (acc, value) => acc + value;
-  const result = array.reduce(reducer);
+  const reducer = (acc, el) => acc + el;
+  const result = array.reduce(reducer, 0);
   return result;
 }
 
 function productAll(array) {
-  const reducer = (acc, value) => acc * value;
-  const result = array.reduce(reducer);
+  const reducer = (acc, el) => acc.concat(el);
+  const reducerProduct = (acc, el) => acc * el;
+  const plain = array.reduce(reducer, []);
+  const result = plain.reduce(reducerProduct);
   return result;
 }
 
 function objectify(array) {
-  // your code here
+  const result = array.reduce((accumulator, currentValue) => {
+    accumulator[currentValue[0]] = currentValue[1];
+    return accumulator;
+  }, {});
+
+  return result;
 }
 
 function luckyNumbers(array) {
