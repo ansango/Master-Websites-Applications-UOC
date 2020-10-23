14a5159f4da2469c4e3b7df5f4bab29cdd68d561
ansango <anibalsantosgo@gmail.com>
2020-10-24 01:00:23 +0200

    js

--- 04 -  Frontend Javascript Frameworks/02 - PECS/02 - PEC - 2/PEC2_Ej1_Array_Methods/PEC2_Ej1/f_zoo/core.js
+++ 04 -  Frontend Javascript Frameworks/02 - PECS/02 - PEC - 2/PEC2_Ej1_Array_Methods/PEC2_Ej1/f_zoo/core.js
@@ -65,35 +65,58 @@ function animalCount(species) {
 }
 
 function animalMap(options) {
+  const animals = JSON.parse(JSON.stringify(data.animals));
   if (typeof options === "undefined") {
-    const animals = JSON.parse(JSON.stringify(data.animals));
-    const obj = {};
-
-    const locations = animals
-      .map((x) => {
-        return x.location;
+    return animals
+      .map((e) => {
+        return {
+          name: e.name,
+          location: e.location,
+        };
       })
-      .filter((el, index, self) => {
-        return index === self.indexOf(el);
-      });
-
-    const arr = [];
-
-    animals.filter((x) => {
-      return x.location;
-    });
-
-    locations.map((x) => {
-      obj[x] = "";
-    });
-
-    //return obj
+      .reduce((a, e) => {
+        if (Array.isArray(a[e.location])) {
+          a[e.location].push(e.name);
+        } else {
+          a[e.location] = [e.name];
+        }
+        return a;
+      }, {});
   } else {
+    const filtered = {};
+    animals.forEach((animal) => {
+      if (!filtered[animal.location]) {
+        filtered[animal.location] = [];
+      }
+      filtered[animal.location].push({
+        // * si aplicamos el filter despues de map desaparece el sexo
+        [animal.name]: animal.residents
+          .filter((r) => (options.sex ? r.sex === options.sex : true))
+          .map((a) => a.name),
+      });
+    });
+    return filtered;
   }
 }
 
 function animalPopularity(rating) {
-  // your code here
+  const animals = JSON.parse(JSON.stringify(data.animals));
+  if (typeof rating === "undefined") {
+    return animals
+      .map((e) => {
+        return {
+          name: e.name,
+          popularity: e.popularity,
+        };
+      })
+      .reduce((a, e) => {
+        if (!Array.isArray(a[e.popularity])) a[e.popularity] = [];
+        a[e.popularity].push(e.name);
+        return a;
+      }, {});
+  } else {
+    return animals.filter((e) => e.popularity === rating).map((e) => e.name);
+  }
 }
 
 function animalsByIds(ids) {
@@ -101,31 +124,104 @@ function animalsByIds(ids) {
     return [];
   } else if (typeof ids === "string") {
     return data.animals.filter((x) => x.id === ids);
-  } else{
-    
+  } else {
+    const animals = JSON.parse(JSON.stringify(data.animals));
+    return animals.filter((animal) => ids.includes(animal.id));
   }
 }
 
 function animalByName(animalName) {
   if (typeof animalName === "undefined") {
     return {};
+  } else {
+    const animals = JSON.parse(JSON.stringify(data.animals));
+    const filtered = animals.find((animal) =>
+      animal.residents.map((resident) => resident.name).includes(animalName)
+    );
+    const animalData = filtered.residents.find(
+      (animal) => animal.name === animalName
+    );
+    return { ...animalData, species: filtered.name };
   }
 }
 
 function employeesByIds(ids) {
-  // your code here
+  if (typeof ids === "undefined") {
+    return [];
+  } else if (typeof ids === "string") {
+    const employees = JSON.parse(JSON.stringify(data.employees));
+    return employees.filter((employee) => employee.id === ids);
+  } else {
+    const employees = JSON.parse(JSON.stringify(data.employees));
+    return employees.filter((employee) => ids.includes(employee.id));
+  }
 }
 
 function employeeByName(employeeName) {
-  // your code here
+  if (typeof employeeName === "undefined") {
+    return {};
+  } else {
+    const employees = JSON.parse(JSON.stringify(data.employees));
+    return employees.find((employee) =>
+      [employee.firstName, employee.lastName].includes(employeeName)
+    );
+  }
 }
 
 function managersForEmployee(idOrName) {
-  // your code here
+  const employees = JSON.parse(JSON.stringify(data.employees));
+
+  // * buscamos el empleado
+
+  const employee = employees.find((e) =>
+    [e.id, e.firstName, e.lastName].includes(idOrName)
+  );
+
+  // * buscamos sus managers
+
+  employee.managers = employee.managers.map((managerId) => {
+    // * buscamos en manager por el id
+
+    const manager = employees.find((e) =>
+      [e.id, e.firstName, e.lastName].includes(managerId)
+    );
+
+    // * retornamos y el valor que se retorna reemplaza el original (el id)
+
+    return `${manager.firstName} ${manager.lastName}`;
+  });
+
+  return employee;
 }
 
 function employeeCoverage(idOrName) {
-  // your code here
+  const employees = JSON.parse(JSON.stringify(data.employees));
+  const animals = JSON.parse(JSON.stringify(data.animals));
+
+  if (typeof idOrName === "undefined") {
+    const _employees = {};
+    employees.forEach((employee) => {
+      _employees[
+        `${employee.firstName} ${employee.lastName}`
+      ] = employee.responsibleFor.map((animalId) => {
+        const _animal = animals.find((a) => a.id === animalId);
+        return _animal.name;
+      });
+    });
+    return _employees;
+  } else {
+    const _employee = {};
+    const employee = employees.find((employee) =>
+      [employee.id, employee.firstName, employee.lastName].includes(idOrName)
+    );
+    _employee[
+      `${employee.firstName} ${employee.lastName}`
+    ] = employee.responsibleFor.map((animalId) => {
+      const _animal = animals.find((a) => a.id === animalId);
+      return _animal.name;
+    });
+    return _employee;
+  }
 }
 
 module.exports = {
