d085e7accc69bc96f74f2653772dd72c6305c98b
Anibal Santos <anibalsantosgo@gmail.com>
2021-02-20 12:08:02 +0100

    ng css

---
+++ 08 - HTML, CSS Tools II/00 - Template tooling/src/assets/scripts/main.js
@@ -0,0 +1,16 @@
+/**
+ * Import dependencies from node_modules
+ * see commented examples below
+ */
+
+// import 'some-node-module';
+// import SomeModule from 'some-node-module';
+
+/**
+ * Write any other JavaScript below
+ */
+
++( function() {
+  const university = "UOC";
+  console.log(`Hello, ${university}!`);
+} )();
